d0160e919324d14b02c17f33599ddf3f81032437
Yuliia Panchenko <yuliiyapanchenko@gmail.com>
2017-11-22 00:46:35 +0200

    app response timeout

--- src/main/java/com/jpanchenko/bookalbumservice/controller/SearchController.java
+++ src/main/java/com/jpanchenko/bookalbumservice/controller/SearchController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static java.util.stream.Collectors.toList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -23,6 +27,10 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 @Slf4j
 @RestController("/")
 public class SearchController {
+
+    @Value("${app.response.timeout}")
+    private int TIMEOUT;
+    private final ScheduledThreadPoolExecutor delayer = new ScheduledThreadPoolExecutor(10);
 
     @Autowired
     List<SearchService> searchServices;
@@ -39,11 +47,14 @@ public class SearchController {
 
     private List<ResponseItem> executeServicesInParallel(String query) {
         List<CompletableFuture<List<ResponseItem>>> futures = searchServices.stream()
-                .map(service -> CompletableFuture.supplyAsync(() -> service.search(query))
-                        .exceptionally(ex -> {
-                            log.error("Unexpected error occurred", ex);
-                            return Collections.emptyList();
-                        }))
+                .map(
+                        service -> CompletableFuture.supplyAsync(() -> service.search(query))
+                                .applyToEitherAsync(timeoutAfter(TIMEOUT, TimeUnit.SECONDS), responseItems -> responseItems)
+                                .exceptionally(ex -> {
+                                    log.error("Unexpected error occurred", ex);
+                                    return Collections.emptyList();
+                                })
+                )
                 .collect(toList());
 
         return futures.stream()
@@ -51,5 +62,11 @@ public class SearchController {
                 .flatMap(Collection::stream)
                 .sorted((o1, o2) -> o1.getTitle().compareTo(o2.getTitle()))
                 .collect(toList());
+    }
+
+    private <T> CompletableFuture<T> timeoutAfter(long timeout, TimeUnit unit) {
+        CompletableFuture<T> result = new CompletableFuture<T>();
+        delayer.schedule(() -> result.completeExceptionally(new TimeoutException("Finished by timeout=" + timeout + unit)), timeout, unit);
+        return result;
     }
 }
